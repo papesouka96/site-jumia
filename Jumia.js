931b2b94520089c14ae370d61b88394cf8c66ffb
pape souka diouf <papesoukad@gmail.com>
2022-09-02 02:14:12 +0200

    slider en bas

--- Jumia.js
+++ Jumia.js
@@ -12,3 +12,19 @@ function ChangeSlide(sens) {
 
 
 
+const productContainers = [...document.querySelectorAll('.hide')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
